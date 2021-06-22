b5584c9829e608b62f5abdbc101e2afab0e3f303
Elmārs Āboliņš <qfluxstudio@gmail.com>
2021-06-22 14:17:35 +0300

    updated for newer helium version

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local helium = require 'helium'
+local scene = helium.scene.new(true)
 
 local elementCreator = helium(function(param, view)
-
 	return function()
 		love.graphics.setColor(0.3, 0.3, 0.3)
 		love.graphics.rectangle('fill', 0, 0, view.w, view.h)
@@ -11,5 +11,13 @@ local elementCreator = helium(function(param, view)
 end)
 
 local element = elementCreator({text = 'foo-bar'}, 100, 20)
-
+--Needs to be called only once, to draw and then :undraw to stop drawing it onscreen
 element:draw(100, 100)
+
+function love.update(dt)
+	scene:update(dt)
+end
+
+function love.draw()
+	scene:draw()
+end
